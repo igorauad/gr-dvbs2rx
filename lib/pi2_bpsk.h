312e3ff1b1303933c7ad0a9fc6fe61e4358b1a9d
Igor Freire <igor.auad@gmail.com>
2021-06-22 22:47:41 -0300

    Extend the comments on derotate_bpsk

--- lib/pi2_bpsk.h
+++ lib/pi2_bpsk.h
@@ -54,15 +54,72 @@ DVBS2RX_API uint64_t demap_bpsk(const gr_complex* in, unsigned int N);
  */
 DVBS2RX_API uint64_t demap_bpsk_diff(const gr_complex* in, unsigned int N);
 
-
 /**
  * @brief Derotate N complex-valued pi/2 BPSK into regular real BPSK symbols
  *
  * Converts a sequence of complex-valued pi/2 BPSK symbols with values
  * originating from the +-0.707 +-j*0.707 constellation points into the
  * corresponding sequence of ordinary real-valued BPSK symbols around +-1. If
- * the input pi/2 BPSK symbols are noisy, naturally the resulting real-valued
- * BPSK symbols are noisy too and deviate from the nominal +-1 values.
+ * the input pi/2 BPSK symbols are noisy, the resulting real-valued BPSK symbols
+ * are naturally noisy too and deviate from the nominal +-1 values.
+ *
+ * This derotation effectively produces the "soft decisions" corresponding to
+ * the received pi/2 BPSK symbols. Taking an even index for the demonstration,
+ * note the constellation symbols are either exp(jpi/4) or exp(-j3pi/4). Hence,
+ * the log-likelihood ratio between the received complex symbol r representing
+ * bit=0 versus bit=1 is as follows:
+ *
+ * LLR(r) =  (-||r - exp(jpi/4)||^2 + ||r - exp(-j3pi/4)||^2) / N0
+ *
+ * Now, let's say r can be expressed as " exp(jpi/4) * r' ", where r' is the
+ * derotated versions of r, namely " r' = r*exp(-jpi/4) ". In this case, it
+ * follows that:
+ *
+ * LLR(r) =  (-||exp(jpi/4)*(r' - 1)||^2 + ||exp(jpi/4)(r' + 1)||^2) / N0,
+ *
+ * given that exp(jpi/4) = -exp(-j3pi/4).
+ *
+ * Since ||exp(jpi/4)||^2 = 1, it can be factored out of the Euclidean norm
+ * terms, which yields:
+ *
+ * LLR(r) =  (-||r' - 1||^2 + ||r' + 1||^2) / N0.
+ *
+ * Besides, note that:
+ *
+ * ||r' - 1||^2 = ||r'||^2 -2*real(<r', 1>) + ||1||^2
+ * ||r' + 1||^2 = ||r'||^2 +2*real(<r', 1>) + ||1||^2
+ *
+ * Hence, the LLR can be expressed as:
+ *
+ * LLR(r) = 4 * real(<r', 1>) / N0,
+ *        = 4 * real(r') / N0.
+ *
+ * In other words, the LLR is given by the real part of the de-rotated symbol
+ * r', scaled by 4/N0. Furthermore, the scaling factor 4/N0 is only useful for a
+ * maximum a posteriori (MAP) decoder, when the constellation symbols are not
+ * equiprobable. In contrast, for maximum likelihood (ML) decoding, when the
+ * symbols are equiprobable, the scaling factor can be ignored, as the threshold
+ * for decision is zero (i.e., bit=0 when LLR(r) > 0 and bit=1 otherwise). Thus,
+ * ultimately, the "soft decisions" are given by real(r'), which is equal to:
+ *
+ * - real(r * exp(-jpi/4)), on even indexes;
+ * - real(r * exp(-j3pi/4)), on odd indexes.
+ *
+ * Note the term soft decision is loosely taken here as a sufficient statistic
+ * indicating the likelihood of a particular bit, which can be used by a
+ * maximum-likelihood decoder. The particular bit-by-bit soft decision that this
+ * function produces is one proportional to the LLR, but not exactly equal to
+ * the LLR (as it does not need to be). Furthermore, using the terminology from
+ * textbooks such as Forney's (Section 6.5.2), the returned value real(r') can
+ * be used to derive both the hard decision "sign(real(r'))" and the reliability
+ * weight associated with this decision given by "abs(real(r'))".
+ *
+ * In the end, what matters the most is the soft decision format expected by the
+ * coding scheme's decoder. Here, we assume the decoder expects real(r'), as
+ * this function is meant to feed soft decisions into the Reed-Muller decoder
+ * (see the implementation of reed_muller::decode on reed_muller.cc). Hence,
+ * this function first derotates each complex input symbol r[k] to produce
+ * r'[k]. Then, it returns real(r'[k]) for all k on the output buffer.
  *
  * @param in (const gr_complex *) Input complex pi/2 BPSK symbols.
  * @param out (float *) Output real-valued BPSK symbols.
