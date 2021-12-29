999915df9e62aa52cbd79ad92322ed452cd0d947
Igor Freire <igor.auad@gmail.com>
2021-12-29 14:15:25 +0000

    Fix /* within block comments on python_bindings.cc
    Clang warns about them.

--- python/bindings/python_bindings.cc
+++ python/bindings/python_bindings.cc
@@ -15,11 +15,11 @@
 namespace py = pybind11;
 
 // Headers for binding functions
-/**************************************/
-/* The following comment block is used for
-/* gr_modtool to insert function prototypes
-/* Please do not delete
-/**************************************/
+/**************************************
+ * The following comment block is used for
+ * gr_modtool to insert function prototypes
+ * Please do not delete
+ **************************************/
 // BINDING_FUNCTION_PROTOTYPES(
 void bind_bbdeheader_bb(py::module& m);
 void bind_bbdescrambler_bb(py::module& m);
@@ -51,11 +51,11 @@ PYBIND11_MODULE(dvbs2rx_python, m)
     // Allow access to base block methods
     py::module::import("gnuradio.gr");
 
-    /**************************************/
-    /* The following comment block is used for
-    /* gr_modtool to insert binding function calls
-    /* Please do not delete
-    /**************************************/
+    /**************************************
+     * The following comment block is used for
+     * gr_modtool to insert binding function calls
+     * Please do not delete
+     **************************************/
     // BINDING_FUNCTION_CALLS(
     bind_bbdeheader_bb(m);
     bind_bbdescrambler_bb(m);
