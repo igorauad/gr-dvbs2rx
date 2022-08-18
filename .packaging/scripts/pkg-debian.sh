6f194ac6c9fb4e9513822e09370be63c1c30ee83
Igor Freire <igor.auad@gmail.com>
2022-08-18 02:39:22 +0000

    packaging: Add script to build deb package

---
+++ .packaging/scripts/pkg-debian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+DEB_REVISION=1 # should start with 1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/pkg-prep.sh
+
+mv $NAME_VER.tar.gz $NAME_VER.orig.tar.gz
+tar xf $NAME_VER.orig.tar.gz
+cd gr-dvbs2rx/
+
+# Move the debian dir to the root dir and fill in the changelog template
+mv .packaging/debian/ .
+sed -i "s/\[%version%\]/$VERSION/g" debian/changelog
+sed -i "s/\[%rev%\]/$DEB_REVISION/g" debian/changelog
+sed -i "s/\[%distroseries%\]/$DIST/g" debian/changelog
+sed -i "s/\[%date%\]/$DATESTR/g" debian/changelog
+
+debuild -uc -us
---
+++ .packaging/scripts/pkg-prep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ex
+
+NAME=gr-dvbs2rx
+ARCH=$(dpkg --print-architecture)
+DATESTR=$(date +"%a, %d %b %Y %T %z")
+DIST=$(lsb_release -s -c)
+BUILD_DIR=/build/gr-dvbs2rx-pkgs/$DIST-$ARCH/
+
+# Cleanup
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+# Scrape the version number from CMakeLists.txt
+VERSION_MAJOR="$(cat CMakeLists.txt | grep "set(VERSION_MAJOR" | tr -s ' ' | cut -d' ' -f2 | cut -d')' -f1)"
+VERSION_API="$(cat CMakeLists.txt | grep "set(VERSION_API" | tr -s ' ' | cut -d' ' -f2 | cut -d')' -f1)"
+VERSION_ABI="$(cat CMakeLists.txt | grep "set(VERSION_ABI" | tr -s ' ' | cut -d' ' -f2 | cut -d')' -f1)"
+VERSION=$VERSION_MAJOR"."$VERSION_API"."$VERSION_ABI
+NAME_VER=$NAME\_$VERSION
+echo "Creating build for $NAME_VER"
+
+# A tag with this version must exist
+git checkout $VERSION
+
+# Archive the sources and git submodules
+git archive --output=$BUILD_DIR/$NAME_VER.tar --prefix=$NAME/ $VERSION
+git submodule foreach "git archive --output $BUILD_DIR/$NAME_VER-\$name.tar --prefix $NAME/\$path/ \$sha1"
+git submodule foreach "tar --concatenate --file $BUILD_DIR/$NAME_VER.tar $BUILD_DIR/$NAME_VER-\$name.tar"
+
+# Prepare the build directory and tarball
+cd $BUILD_DIR
+gzip --no-name $NAME_VER.tar
