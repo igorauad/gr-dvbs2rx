641cf6140ad78ffcc26235ed129ef3f493ab26b3
Igor Freire <igor.auad@gmail.com>
2023-07-11 14:13:15 +0000

    Add missing array include

--- lib/plsync_cc_impl.h
+++ lib/plsync_cc_impl.h
@@ -17,6 +17,7 @@
 #include "pl_signaling.h"
 #include <gnuradio/dvbs2rx/plsync_cc.h>
 #include <volk/volk_alloc.hh>
+#include <array>
 #include <queue>
 
 namespace gr {
@@ -46,7 +47,7 @@ struct rot_ctrl_t {
     rot_state_t current;         /** Frequency state at the current PLFRAME */
     std::queue<tag_t> tag_queue; /** Queue of rot_phase_inc tags */
     std::map<uint64_t, rot_phase_adj_t>
-        update_map; /** Map of scheduled phase increment updates */
+        update_map;              /** Map of scheduled phase increment updates */
 };
 
 /** @brief Index tracking for various segments of a PLFRAME */
