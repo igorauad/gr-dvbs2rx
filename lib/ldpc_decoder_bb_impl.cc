f6dc7b4f5657c4e17e4a263749582633c7c90bb5
Igor Freire <igor.auad@gmail.com>
2023-12-01 21:35:58 +0000

    ldpc: Move CPU feature resolution to constructor
    The previous implementation was leading to a stack smashing error
    detected on RPM package builds.

--- lib/ldpc_decoder_bb_impl.cc
+++ lib/ldpc_decoder_bb_impl.cc
@@ -24,18 +24,11 @@
 #ifdef CPU_FEATURES_ARCH_ARM
 #include "cpuinfo_arm.h"
 using namespace cpu_features;
-static const ArmFeatures features = GetArmInfo().features;
-static const bool has_neon = features.neon;
-#endif
-
-#ifdef CPU_FEATURES_ARCH_AARCH64
-static const bool has_neon = true; // always available on aarch64
 #endif
 
 #ifdef CPU_FEATURES_ARCH_X86
 #include "cpuinfo_x86.h"
 using namespace cpu_features;
-static const X86Features features = GetX86Info().features;
 #endif
 
 namespace ldpc_neon {
@@ -57,234 +50,6 @@ namespace ldpc_generic {
 void ldpc_dec_init(LDPCInterface* it);
 int ldpc_dec_decode(void* buffer, int8_t* code, int trials);
 } // namespace ldpc_generic
-
-constexpr int DVB_S2_TABLE_B1::DEG[];
-constexpr int DVB_S2_TABLE_B1::LEN[];
-constexpr int DVB_S2_TABLE_B1::POS[];
-
-constexpr int DVB_S2_TABLE_B2::DEG[];
-constexpr int DVB_S2_TABLE_B2::LEN[];
-constexpr int DVB_S2_TABLE_B2::POS[];
-
-constexpr int DVB_S2_TABLE_B3::DEG[];
-constexpr int DVB_S2_TABLE_B3::LEN[];
-constexpr int DVB_S2_TABLE_B3::POS[];
-
-constexpr int DVB_S2_TABLE_B4::DEG[];
-constexpr int DVB_S2_TABLE_B4::LEN[];
-constexpr int DVB_S2_TABLE_B4::POS[];
-
-constexpr int DVB_S2_TABLE_B5::DEG[];
-constexpr int DVB_S2_TABLE_B5::LEN[];
-constexpr int DVB_S2_TABLE_B5::POS[];
-
-constexpr int DVB_S2_TABLE_B6::DEG[];
-constexpr int DVB_S2_TABLE_B6::LEN[];
-constexpr int DVB_S2_TABLE_B6::POS[];
-
-constexpr int DVB_S2_TABLE_B7::DEG[];
-constexpr int DVB_S2_TABLE_B7::LEN[];
-constexpr int DVB_S2_TABLE_B7::POS[];
-
-constexpr int DVB_S2_TABLE_B8::DEG[];
-constexpr int DVB_S2_TABLE_B8::LEN[];
-constexpr int DVB_S2_TABLE_B8::POS[];
-
-constexpr int DVB_S2_TABLE_B9::DEG[];
-constexpr int DVB_S2_TABLE_B9::LEN[];
-constexpr int DVB_S2_TABLE_B9::POS[];
-
-constexpr int DVB_S2_TABLE_B10::DEG[];
-constexpr int DVB_S2_TABLE_B10::LEN[];
-constexpr int DVB_S2_TABLE_B10::POS[];
-
-constexpr int DVB_S2_TABLE_B11::DEG[];
-constexpr int DVB_S2_TABLE_B11::LEN[];
-constexpr int DVB_S2_TABLE_B11::POS[];
-
-constexpr int DVB_S2X_TABLE_B1::DEG[];
-constexpr int DVB_S2X_TABLE_B1::LEN[];
-constexpr int DVB_S2X_TABLE_B1::POS[];
-
-constexpr int DVB_S2X_TABLE_B2::DEG[];
-constexpr int DVB_S2X_TABLE_B2::LEN[];
-constexpr int DVB_S2X_TABLE_B2::POS[];
-
-constexpr int DVB_S2X_TABLE_B3::DEG[];
-constexpr int DVB_S2X_TABLE_B3::LEN[];
-constexpr int DVB_S2X_TABLE_B3::POS[];
-
-constexpr int DVB_S2X_TABLE_B4::DEG[];
-constexpr int DVB_S2X_TABLE_B4::LEN[];
-constexpr int DVB_S2X_TABLE_B4::POS[];
-
-constexpr int DVB_S2X_TABLE_B5::DEG[];
-constexpr int DVB_S2X_TABLE_B5::LEN[];
-constexpr int DVB_S2X_TABLE_B5::POS[];
-
-constexpr int DVB_S2X_TABLE_B6::DEG[];
-constexpr int DVB_S2X_TABLE_B6::LEN[];
-constexpr int DVB_S2X_TABLE_B6::POS[];
-
-constexpr int DVB_S2X_TABLE_B7::DEG[];
-constexpr int DVB_S2X_TABLE_B7::LEN[];
-constexpr int DVB_S2X_TABLE_B7::POS[];
-
-constexpr int DVB_S2X_TABLE_B8::DEG[];
-constexpr int DVB_S2X_TABLE_B8::LEN[];
-constexpr int DVB_S2X_TABLE_B8::POS[];
-
-constexpr int DVB_S2X_TABLE_B9::DEG[];
-constexpr int DVB_S2X_TABLE_B9::LEN[];
-constexpr int DVB_S2X_TABLE_B9::POS[];
-
-constexpr int DVB_S2X_TABLE_B10::DEG[];
-constexpr int DVB_S2X_TABLE_B10::LEN[];
-constexpr int DVB_S2X_TABLE_B10::POS[];
-
-constexpr int DVB_S2X_TABLE_B11::DEG[];
-constexpr int DVB_S2X_TABLE_B11::LEN[];
-constexpr int DVB_S2X_TABLE_B11::POS[];
-
-constexpr int DVB_S2X_TABLE_B12::DEG[];
-constexpr int DVB_S2X_TABLE_B12::LEN[];
-constexpr int DVB_S2X_TABLE_B12::POS[];
-
-constexpr int DVB_S2X_TABLE_B13::DEG[];
-constexpr int DVB_S2X_TABLE_B13::LEN[];
-constexpr int DVB_S2X_TABLE_B13::POS[];
-
-constexpr int DVB_S2X_TABLE_B14::DEG[];
-constexpr int DVB_S2X_TABLE_B14::LEN[];
-constexpr int DVB_S2X_TABLE_B14::POS[];
-
-constexpr int DVB_S2X_TABLE_B15::DEG[];
-constexpr int DVB_S2X_TABLE_B15::LEN[];
-constexpr int DVB_S2X_TABLE_B15::POS[];
-
-constexpr int DVB_S2X_TABLE_B16::DEG[];
-constexpr int DVB_S2X_TABLE_B16::LEN[];
-constexpr int DVB_S2X_TABLE_B16::POS[];
-
-constexpr int DVB_S2X_TABLE_B17::DEG[];
-constexpr int DVB_S2X_TABLE_B17::LEN[];
-constexpr int DVB_S2X_TABLE_B17::POS[];
-
-constexpr int DVB_S2X_TABLE_B18::DEG[];
-constexpr int DVB_S2X_TABLE_B18::LEN[];
-constexpr int DVB_S2X_TABLE_B18::POS[];
-
-constexpr int DVB_S2X_TABLE_B19::DEG[];
-constexpr int DVB_S2X_TABLE_B19::LEN[];
-constexpr int DVB_S2X_TABLE_B19::POS[];
-
-constexpr int DVB_S2X_TABLE_B20::DEG[];
-constexpr int DVB_S2X_TABLE_B20::LEN[];
-constexpr int DVB_S2X_TABLE_B20::POS[];
-
-constexpr int DVB_S2X_TABLE_B21::DEG[];
-constexpr int DVB_S2X_TABLE_B21::LEN[];
-constexpr int DVB_S2X_TABLE_B21::POS[];
-
-constexpr int DVB_S2X_TABLE_B22::DEG[];
-constexpr int DVB_S2X_TABLE_B22::LEN[];
-constexpr int DVB_S2X_TABLE_B22::POS[];
-
-constexpr int DVB_S2X_TABLE_B23::DEG[];
-constexpr int DVB_S2X_TABLE_B23::LEN[];
-constexpr int DVB_S2X_TABLE_B23::POS[];
-
-constexpr int DVB_S2X_TABLE_B24::DEG[];
-constexpr int DVB_S2X_TABLE_B24::LEN[];
-constexpr int DVB_S2X_TABLE_B24::POS[];
-
-constexpr int DVB_S2_TABLE_C1::DEG[];
-constexpr int DVB_S2_TABLE_C1::LEN[];
-constexpr int DVB_S2_TABLE_C1::POS[];
-
-constexpr int DVB_S2_TABLE_C2::DEG[];
-constexpr int DVB_S2_TABLE_C2::LEN[];
-constexpr int DVB_S2_TABLE_C2::POS[];
-
-constexpr int DVB_S2_TABLE_C3::DEG[];
-constexpr int DVB_S2_TABLE_C3::LEN[];
-constexpr int DVB_S2_TABLE_C3::POS[];
-
-constexpr int DVB_S2_TABLE_C4::DEG[];
-constexpr int DVB_S2_TABLE_C4::LEN[];
-constexpr int DVB_S2_TABLE_C4::POS[];
-
-constexpr int DVB_S2_TABLE_C5::DEG[];
-constexpr int DVB_S2_TABLE_C5::LEN[];
-constexpr int DVB_S2_TABLE_C5::POS[];
-
-constexpr int DVB_S2_TABLE_C6::DEG[];
-constexpr int DVB_S2_TABLE_C6::LEN[];
-constexpr int DVB_S2_TABLE_C6::POS[];
-
-constexpr int DVB_S2_TABLE_C7::DEG[];
-constexpr int DVB_S2_TABLE_C7::LEN[];
-constexpr int DVB_S2_TABLE_C7::POS[];
-
-constexpr int DVB_S2_TABLE_C8::DEG[];
-constexpr int DVB_S2_TABLE_C8::LEN[];
-constexpr int DVB_S2_TABLE_C8::POS[];
-
-constexpr int DVB_S2_TABLE_C9::DEG[];
-constexpr int DVB_S2_TABLE_C9::LEN[];
-constexpr int DVB_S2_TABLE_C9::POS[];
-
-constexpr int DVB_S2_TABLE_C10::DEG[];
-constexpr int DVB_S2_TABLE_C10::LEN[];
-constexpr int DVB_S2_TABLE_C10::POS[];
-
-constexpr int DVB_S2X_TABLE_C1::DEG[];
-constexpr int DVB_S2X_TABLE_C1::LEN[];
-constexpr int DVB_S2X_TABLE_C1::POS[];
-
-constexpr int DVB_S2X_TABLE_C2::DEG[];
-constexpr int DVB_S2X_TABLE_C2::LEN[];
-constexpr int DVB_S2X_TABLE_C2::POS[];
-
-constexpr int DVB_S2X_TABLE_C3::DEG[];
-constexpr int DVB_S2X_TABLE_C3::LEN[];
-constexpr int DVB_S2X_TABLE_C3::POS[];
-
-constexpr int DVB_S2X_TABLE_C4::DEG[];
-constexpr int DVB_S2X_TABLE_C4::LEN[];
-constexpr int DVB_S2X_TABLE_C4::POS[];
-
-constexpr int DVB_S2X_TABLE_C5::DEG[];
-constexpr int DVB_S2X_TABLE_C5::LEN[];
-constexpr int DVB_S2X_TABLE_C5::POS[];
-
-constexpr int DVB_S2X_TABLE_C6::DEG[];
-constexpr int DVB_S2X_TABLE_C6::LEN[];
-constexpr int DVB_S2X_TABLE_C6::POS[];
-
-constexpr int DVB_S2X_TABLE_C7::DEG[];
-constexpr int DVB_S2X_TABLE_C7::LEN[];
-constexpr int DVB_S2X_TABLE_C7::POS[];
-
-constexpr int DVB_S2X_TABLE_C8::DEG[];
-constexpr int DVB_S2X_TABLE_C8::LEN[];
-constexpr int DVB_S2X_TABLE_C8::POS[];
-
-constexpr int DVB_S2X_TABLE_C9::DEG[];
-constexpr int DVB_S2X_TABLE_C9::LEN[];
-constexpr int DVB_S2X_TABLE_C9::POS[];
-
-constexpr int DVB_S2X_TABLE_C10::DEG[];
-constexpr int DVB_S2X_TABLE_C10::LEN[];
-constexpr int DVB_S2X_TABLE_C10::POS[];
-
-constexpr int DVB_T2_TABLE_A3::DEG[];
-constexpr int DVB_T2_TABLE_A3::LEN[];
-constexpr int DVB_T2_TABLE_A3::POS[];
-
-constexpr int DVB_T2_TABLE_B3::DEG[];
-constexpr int DVB_T2_TABLE_B3::LEN[];
-constexpr int DVB_T2_TABLE_B3::POS[];
 
 namespace gr {
 namespace dvbs2rx {
@@ -545,6 +310,12 @@ ldpc_decoder_bb_impl::ldpc_decoder_bb_impl(dvb_standard_t standard,
     std::string impl = "generic";
 #ifdef CPU_FEATURES_ARCH_ANY_ARM
     d_simd_size = 16;
+#ifdef CPU_FEATURES_ARCH_AARCH64
+    const bool has_neon = true; // always available on aarch64
+#else
+    const ArmFeatures features = GetArmInfo().features;
+    const bool has_neon = features.neon;
+#endif
     if (has_neon) {
         ldpc_neon::ldpc_dec_init(d_ldpc);
         decode = &ldpc_neon::ldpc_dec_decode;
@@ -555,6 +326,7 @@ ldpc_decoder_bb_impl::ldpc_decoder_bb_impl(dvb_standard_t standard,
     }
 #else
 #ifdef CPU_FEATURES_ARCH_X86
+    const X86Features features = GetX86Info().features;
     d_simd_size = features.avx2 ? 32 : 16;
     if (features.avx2) {
         ldpc_avx2::ldpc_dec_init(d_ldpc);
